ff69d20548adb254f272a0b42bf2de465e030c25
Rodri <Rodri@RODRIGO>
2024-12-01 21:49:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula idade !");
+            Pessoa p = new Pessoa();
+            Console.Write ("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write ("Ano de Nascimento:");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            p.ExibirDados();
+        }
+    }
+}
